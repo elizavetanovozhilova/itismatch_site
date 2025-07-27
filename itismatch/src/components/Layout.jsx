9c89bf4d5d3452c96cd5469744b71097ada9b895
elizavetanovozilova <babygorilla@yandex.ru>
2025-07-27 18:16:25 +0300

    vercel json

--- itismatch/src/components/Layout.jsx
+++ itismatch/src/components/Layout.jsx
@@ -3,8 +3,8 @@ import { Outlet } from 'react-router-dom'
 
 function Layout() {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', height: '90vh' }}>
-      <div style={{ flex: 1 }}>
+    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', height: '100vh', background: 'var(--white)' }}>
+      <div style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
         <Outlet />
       </div>
       <Footer />
